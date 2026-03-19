274ea687cbdc454fb4528beca793f577fd580390
Milana Ustinova <131642299+MyatnoeVino@users.noreply.github.com>
2026-03-19 12:46:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const MEESKOND = process.env.TEAM_NAME || "Tundmatu tiim (Viga!)";
 
-// Главная страница
-app.get('/', (req, res) => {
-    res.send(`<h1>Сервер работает!</h1><p>Meeskond: ${MEESKOND}</p>`);
-});
-
-// API-информация
 app.get('/api/info', (req, res) => {
     res.status(200).json({ 
         missioon: "Iseseisev deploimine edukas",
@@ -18,7 +12,6 @@ app.get('/api/info', (req, res) => {
     });
 });
 
-// Запуск сервера только один раз
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`API Server töötab selle pordi peale: ${PORT}`);
+    console.log(`API Server tootab selle pordi peale: ${PORT}`);
 });
