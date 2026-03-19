db69ddd9fbaa7418aa1acbb9d5087d9163680fab
Milana Ustinova <131642299+MyatnoeVino@users.noreply.github.com>
2026-03-19 13:31:21 +0200

    Refactor server.js to improve API structure

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
+const express = require("express");
+const app = express();
+
 const PORT = process.env.PORT || 3000;
-const MEESKOND = process.env.TEAM_NAME || "Tundmatu tiim (Viga!)";
+const TEAM_NAME = process.env.TEAM_NAME || "Tundmatu tiim (Viga!)";
 
-app.get('/', (req, res) => {
-    res.send(`<h1>Сервер работает!</h1><p>Meeskond: ${MEESKOND}</p>`);
+app.get("/api/info", (req, res) => {
+  res.json({
+    meeskond: TEAM_NAME
+  });
 });
 
-app.get('/api/info', (req, res) => {
-    res.status(200).json({ 
-        missioon: "Iseseisev deploimine edukas",
-@@ -13,12 +18,7 @@ app.get('/api/info', (req, res) => {
-    });
-});
-
-app.get('/', (req, res) => {
-    res.send(`<h1>Server töötab!</h1><p>Meeskond: ${MEESKOND}</p>`);
-});
-
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`API Server töötab selle pordi peale: ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server töötab!");
 });
